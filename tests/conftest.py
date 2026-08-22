50b0b18e325e6849103ab7c2a2e1564adf077725
Mazen Aly <mazenaly256@gmail.com>
2026-08-22 11:02:25 +0300

    chore: add the test fixture function to upgrade the database to the latest version according to the versions folder

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,21 @@
 import pytest
-
 from testcontainers.community.postgres import PostgresContainer
+
+from alembic import command
+from alembic.config import Config
 
 @pytest.fixture(scope="session")    # runs at the beginning of each testing session and reuse the same object for every test
 def postgres_server_container():
     with PostgresContainer("postgres:16") as container:
         yield container
+
+
+
+@pytest.fixture(scope="session")
+def apply_migrations(postgres_server_container):
+    connection_url = postgres_server_container.get_connection_url()     # this is how we can connect the application with the containerized db server
+
+    alembic_cfg = Config("alembic.ini")
+    alembic_cfg.set_main_option("sqlalchemy.url", connection_url)
+
+    command.upgrade(alembic_cfg, "head")
