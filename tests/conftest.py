cd2c285d1a8766462d8a67d9d6da02fb90a130ba
Mazen Aly <mazenaly256@gmail.com>
2026-08-22 13:17:04 +0300

    chore: finish setting up the fixtures/dependencies of the testing functions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,16 @@
 import pytest
 from testcontainers.community.postgres import PostgresContainer
 
+import pytest_asyncio
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+
 from alembic import command
 from alembic.config import Config
+
+from httpx import AsyncClient, ASGITransport
+
+from app.main import app
+from app.core.database import get_db
 
 @pytest.fixture(scope="session")    # runs at the beginning of each testing session and reuse the same object for every test
 def postgres_server_container():
@@ -13,9 +21,58 @@ def postgres_server_container():
 
 @pytest.fixture(scope="session")
 def apply_migrations(postgres_server_container):
-    connection_url = postgres_server_container.get_connection_url()     # this is how we can connect the application with the containerized db server
+    connection_url = postgres_server_container.get_connection_url().replace(
+        "postgresql://", "postgresql+asyncpg://"
+    )     # this is how we can connect the application with the containerized db server
 
     alembic_cfg = Config("alembic.ini")
     alembic_cfg.set_main_option("sqlalchemy.url", connection_url)
 
     command.upgrade(alembic_cfg, "head")
+
+
+
+@pytest_asyncio.fixture(scope="session")    # creates an engine (that creates and manages the connections with db, the connection pool) once per testing session
+async def test_engine(postgres_server_container, apply_migrations):
+    connection_url = postgres_server_container.get_connection_url().replace(
+        "postgresql://", "postgresql+asyncpg://"
+    )
+    engine = create_async_engine(connection_url)
+
+    yield engine
+
+    await engine.dispose()
+
+
+
+@pytest_asyncio.fixture     # does real async DB work, and runs for every single testing function
+async def db_session(test_engine):
+    async with test_engine.connect() as connection:
+        outer_transaction = await connection.begin()    # this is always rolled back to clear the changes that happened during the test
+
+        session = AsyncSession(bind=connection, join_transaction_mode="create_savepoint")
+
+        yield session
+
+        await session.close()
+        await outer_transaction.rollback()
+
+
+
+
+@pytest_asyncio.fixture
+async def client(db_session):
+
+    async def override_get_db():
+        return db_session
+
+    app.dependency_overrides[get_db] = override_get_db
+
+    transport = ASGITransport(app=app)
+
+    async with AsyncClient(transport=transport, base_url="http://test") as async_client:    # takes base_url as there is no real server like what uvicorn introduces,
+                                                                                            # the client is in the same process as the application, so it calls it directly like an ordinary function
+                                                                                            # there is not any network involved
+        yield async_client
+
+    app.dependency_overrides.clear()    # just for cleanliness
