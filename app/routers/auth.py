e70799b71caaafc5fb2e891b1aed4b837c3572b7
Mazen Aly <mazenaly256@gmail.com>
2026-08-22 08:58:50 +0300

    fix: force repeated username or email to return 409 conflict instead throwing exception and returning 500

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,6 @@
 import bcrypt
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.exc import IntegrityError
 from starlette import status
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.core.database import get_db
@@ -11,7 +12,11 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 @router.post(
     path="/register",
     status_code=status.HTTP_201_CREATED,
-    response_model=UserRead
+    response_model=UserRead,
+    responses={
+        status.HTTP_409_CONFLICT: {"description": "Email/Username Already Exists"},
+        status.HTTP_201_CREATED: {"description": "Successful Registration"}
+    }
 )
 async def register(user_request_model: UserCreate, db: AsyncSession = Depends(get_db)):
     new_user = User(
@@ -21,7 +26,17 @@ async def register(user_request_model: UserCreate, db: AsyncSession = Depends(ge
     )
 
     db.add(new_user)
-    await db.commit()
+
+    try:
+        await db.commit()
+
+    except IntegrityError:
+        await db.rollback()     # clears SQLAlchemy's in-memory pending state and sends ROLLBACK to Postgres,
+                                # discarding the aborted transaction so the underlying connection is safe to reuse (by this session or, once pooled, another)
+
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Email or username are already registered")
+
+
     await db.refresh(new_user)
 
     return UserRead(id=new_user.id, email=new_user.email, username=new_user.username)
