d4ceaab44b939c0ac970a4e6b2f0678a2728d1d6
Mazen Aly <mazenaly256@gmail.com>
2026-09-08 19:28:06 +0300

    feat: change the FK constraints to on delete cascade behavior, to allow easier deleting for a resource and all its children

--- app/models/board_column.py
+++ app/models/board_column.py
@@ -7,6 +7,6 @@ class BoardColumn(Base):
     __tablename__ = "board_columns"
 
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    board_id: Mapped[int] = mapped_column(ForeignKey("boards.id"))
+    board_id: Mapped[int] = mapped_column(ForeignKey("boards.id", ondelete="CASCADE"))
     title: Mapped[str]
     index: Mapped[float]
