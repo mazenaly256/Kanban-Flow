6884932b5f36504736159c733be24d2e66bfd407
Mazen Aly <mazenaly256@gmail.com>
2026-09-08 17:49:55 +0300

    feat: add create board DTO, and extend the get board DTO to include id

--- app/schemas/board.py
+++ app/schemas/board.py
@@ -3,5 +3,11 @@ from typing import Literal
 from pydantic import BaseModel
 
 class BoardRead(BaseModel):
+    board_id: int
     board_title: str
     role: Literal["owner", "manager", "assignee", "viewer"]
+
+
+
+class BoardCreate(BaseModel):
+    board_title: str
