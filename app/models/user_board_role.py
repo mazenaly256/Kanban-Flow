26de41722a6d381e83b5c8a20d3e7d4956eca7dc
Mazen Aly <mazenaly256@gmail.com>
2026-09-09 09:57:09 +0300

    feat: add on delete cascade on FK, to auto delete the user-board role on deleting of user or board

--- app/models/user_board_role.py
+++ app/models/user_board_role.py
@@ -6,6 +6,6 @@ from app.core.database import Base
 class UserBoardRole(Base):
     __tablename__ = "user_board_roles"
 
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
-    board_id: Mapped[int] = mapped_column(ForeignKey("boards.id"), primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+    board_id: Mapped[int] = mapped_column(ForeignKey("boards.id", ondelete="CASCADE"), primary_key=True)
     role: Mapped[str]
