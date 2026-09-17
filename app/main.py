ae00da8f1dc1b9046e98d449d2167f7d84a08086
Mazen Aly <mazenaly256@gmail.com>
2026-09-17 20:54:59 +0300

    feat: implement jwt validation logic and close the connection when the token is invalid or expired and the previous failing test passes (Green Phase)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 import asyncio
+import json
+from app.core.security import decode_jwt_access_token
 
 from fastapi import FastAPI
 from starlette.websockets import WebSocket
@@ -16,11 +18,19 @@ app.include_router(tasks.router)
 
 
 @app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket_endpoint(websocket: WebSocket):     # executed once per connection
     await websocket.accept()  # server accepts to upgrade the connection to websockets (accepting the handshake and responding with HTTP 101)
 
     try:
-        auth_message = await asyncio.wait_for(websocket.receive_text(), timeout=10)     # wait in the background, either text is received or the timeout is reached and throws asyncio.TimeoutError exception
+        message = await asyncio.wait_for(websocket.receive_text(), timeout=10)     # wait in the background, either text is received or the timeout is reached and throws asyncio.TimeoutError exception
+
+        data = json.loads(message)
+        jwt = data.get("token")
+
+        user = decode_jwt_access_token(jwt)
+
+        if user is None:
+            await websocket.close(code=1008, reason="Invalid token")    # '.close()' sends the closing message/frame to the client
 
     except asyncio.TimeoutError:
         await websocket.close(code=1008, reason="Authentication timeout")   # sends the last message, that is the closing of connection
