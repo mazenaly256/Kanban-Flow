3e953e53af1d2e7c7e44a6d1757650ba69941e9c
Mazen Aly <mazenaly256@gmail.com>
2026-09-17 19:32:39 +0300

    feat: add the endpoint of websocket connection and automatically close the connection after the timeout if no jwt reached, and test passes (Green Phase)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
+import asyncio
+
 from fastapi import FastAPI
+from starlette.websockets import WebSocket
+
 from app.routers import health, auth, users, boards, board_columns, tasks
 
 app = FastAPI()
@@ -9,3 +13,14 @@ app.include_router(users.router)
 app.include_router(boards.router)
 app.include_router(board_columns.router)
 app.include_router(tasks.router)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()  # server accepts to upgrade the connection to websockets (accepting the handshake and responding with HTTP 101)
+
+    try:
+        auth_message = await asyncio.wait_for(websocket.receive_text(), timeout=10)     # wait in the background, either text is received or the timeout is reached and throws asyncio.TimeoutError exception
+
+    except asyncio.TimeoutError:
+        await websocket.close(code=1008, reason="Authentication timeout")   # sends the last message, that is the closing of connection
