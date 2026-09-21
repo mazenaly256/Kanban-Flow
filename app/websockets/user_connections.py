8f474ab9a02434675ac226e457d711485a0ea422
Mazen Aly <mazenaly256@gmail.com>
2026-09-21 07:53:40 +0300

    feat: implement a registery for all opened websocket connections with all users connected to the server

---
+++ app/websockets/user_connections.py
@@ -0,0 +1,21 @@
+from starlette.websockets import WebSocket
+
+
+class UserConnections:
+    def __init__(self):
+        # connections dictionary is defined here to be fabulous
+        self._connections: dict[int, set[WebSocket]] = {}  # set is used for fast removal, as order is not important here
+
+    def add(self, user_id: int, ws: WebSocket):
+        if user_id not in self._connections:   # checks if user id is a key in the dictionary
+            self._connections[user_id] = set()
+
+        self._connections[user_id].add(ws)
+
+
+    def remove(self, user_id: int, ws: WebSocket):
+        if user_id in self._connections:
+            self._connections[user_id].discard(ws)
+
+            if not self._connections[user_id]:      # if the set is empty
+                del self._connections[user_id]      # then delete the key and value from the memory to clean up
